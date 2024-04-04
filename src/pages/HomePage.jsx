36696420af7a7c192118a0fc18ef43151ba555af
Ben <bnk989@gmail.com>
2024-04-04 19:29:47 +0300

    Homepage update - hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { SearchBox } from '../cmps/SearchBox'
 // import { CHANGE_COUNT } from '../store/user.reducer'
 
 
@@ -11,8 +12,22 @@ export function HomePage() {
     return (
         <section className='homepage-section'>
             <section className='hero-container main-layout full'>
-                <img src="../../public/img/hero/hero-img1.webp" alt="hero image" />
+                {/* <img src="../../public/img/hero/hero-img1.webp" alt="hero image" /> */}
+                <div class="search-container flex">
+                    <h1>Find the right <i>freelance</i><br/> service, right away</h1>
+                <SearchBox/>
+                <div className="popular">
+                    <ul className='clean-list flex'>
+                        <li>Popular:</li>
+                        <li>Website Design</li>
+                        <li>wordPress</li>
+                        <li>Logo Design</li>
+                        <li>AI Services</li>
+                    </ul>
+                </div>
+                </div>
             </section>
+            
             <h2>HomePage page</h2>
             <h1>hay</h1>
             <ul>
