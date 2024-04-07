4b4d9e2451db3ba7ad70331151eeb69ec5118ae4
Ben <bnk989@gmail.com>
2024-04-07 08:09:58 +0300

    Added Submenu for Explore

--- src/cmps/Navbar.jsx
+++ src/cmps/Navbar.jsx
@@ -1,13 +1,32 @@
 import { Link, NavLink } from 'react-router-dom'
 import { LoginSignup } from "./LoginSignup"
-import {NavRoutes} from '../routes'
+import {NavRoutes, exploreMenu} from '../routes'
 import { Button } from '@/components/ui/button'
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
 
 export function NavBar() {
     return (
         <nav className="fiverr-nav">
             <ul className='clean-list flex'>
-                {NavRoutes.map(route => <li key={route.path} className={route.path}><NavLink to={route.path}>{route.label}</NavLink></li>)}
+                <li className='explore cursor-pointer' ><DropdownMenu className='cursor-pointer'>
+  <DropdownMenuTrigger>Explore</DropdownMenuTrigger>
+  <DropdownMenuContent>
+  {exploreMenu.map(menu =>
+    <DropdownMenuItem key={menu.label}>
+        {menu.label}<br/>{menu.subText}
+    </DropdownMenuItem>)}
+  </DropdownMenuContent>
+</DropdownMenu></li>    
+                {NavRoutes.splice(1,2).map(route => <li key={route.path} className={route.path}><NavLink to={route.path}>{route.label}</NavLink></li>)}
                 {/* <LoginSignup /> */}
                 <li>Sign in</li>
                 <li className='shancn-btn'><Button variant="outline" className='bg-inherit'>Join</Button></li>
