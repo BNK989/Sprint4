6f81732cfce9a735baa6859a75eb7832916ae25d
meni-kr <meni.kramer@gmail.com>
2024-04-07 22:43:07 +0300

    User menu add click outside to close

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import { GigIndex } from './pages/GigIndex.jsx'
 import { ReviewIndex } from './pages/ReviewIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
 import { GigDetails } from './cmps/GigDetails.jsx'
+import { UserDetails } from './pages/UserDetails.jsx'
 
 // Routes accessible from the main navigation (in AppHeader)
 const routes = [
@@ -30,6 +31,11 @@ const routes = [
         path: 'explore/:gigId',
         component: <GigDetails />,
         label: 'details'
+    },
+    {
+        path: 'user/:userId',
+        component: <UserDetails />,
+        label: 'user'
     },
 ]
 export default routes
