73910e1ca4720222c467836563bdbea91a4f0b32
aviya-ha <aviyaharel6@gmail.com>
2024-04-08 23:12:40 +0300

    Add preview

--- src/cmps/GigDetails.jsx
+++ src/cmps/GigDetails.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { gigService } from "../services/gig.service.local";
 import { Link } from "react-router-dom";
+import { ReviewsStats } from "./ReviewsStats";
 
 
 export function GigDetails() {
     const { gigId } = useParams()
     const navigate = useNavigate()
     const [gig, setGig] = useState(null)
+    
 
     console.log('gigId:', gigId)
 
@@ -25,6 +27,34 @@ export function GigDetails() {
             showErrorMsg('Cannot load gig')
             navigate('/explore')
         }
+    }
+
+    function showRate(review) {
+        let stars = [false, false, false, false, false]
+        const count = review.rate
+
+        for (let i = 0; i < count; i++) {
+            stars[i] = true
+        }
+        return stars
+    }
+
+    function stateStats(){
+       const totalReviews = gig.reviews.length
+       const fiveStars = gig.reviews.filter(review => review.rate === 5).length
+       const fourStars = gig.reviews.filter(review => review.rate === 4).length
+       const threeStars = gig.reviews.filter(review => review.rate === 3).length
+       const twoStars = gig.reviews.filter(review => review.rate === 2).length
+       const oneStar = gig.reviews.filter(review => review.rate === 1).length
+       console.log('totalReviews:', totalReviews)
+       console.log('fiveStars:', fiveStars)
+       console.log('fourStars:', fourStars)
+       console.log('threeStars:', threeStars)
+       console.log('twoStars:', twoStars)
+       console.log('oneStar:', oneStar)
+
+       return { totalReviews, fiveStars, fourStars, threeStars, twoStars, oneStar }
+        
     }
 
     if (!gig) return <h1>Loading</h1>
@@ -52,7 +82,7 @@ export function GigDetails() {
 
                 <section className="about-this-gig">
                     <p>About this gig</p>
-                    {/* <pre>{gig.description}</pre> */}
+                    {/* <span>{gig.description}</span> */}
                     Lorem ipsum dolor sit amet,
                     consectetur adipisicing elit.
                     Consequuntur enim, officia soluta accusantium quaerat
@@ -67,7 +97,7 @@ export function GigDetails() {
                 <div className="btns">
                     <button className="btn-basic">Basic</button>
                     <button className="btn-standard">Standard</button>
-                    <button className="btn-premium">Premium</button>
+                    <button className="btn-spanmium">spanmium</button>
                 </div>
 
                 <main className="main-details">
@@ -102,9 +132,62 @@ export function GigDetails() {
                 </main>
 
             </section>
-
+            {/* {
+        "id": "127",
+        "txt": "Excellent work, very professional and easy to work with",
+        "rate": 5,
+        "by": {
+          "_id": "u103",
+          "fullname": "Yonatan Biton",
+          "imgUrl": "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
+        }
+      }, */}
             <section className="order-reviews-container">
-                
+                <div className="title">Reviews</div>
+
+<ReviewsStats stateStats={stateStats()}/>
+
+                <ul>
+                    {gig.reviews.map((review, idx) => (
+                        <li key={idx}>
+                            <div className="line"></div>
+                            <div className="by">
+                                <img src={review.by.imgUrl} alt="owner image" />
+
+                                <div className="user-info">
+
+                                    <span className="user-name">{review.by.fullname}</span>
+
+                                    <div className="from">
+                                        <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1ee-1f1f1.png" alt="" />
+                                        <span>Israel</span>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div className="rate">
+                                {showRate(review).map((value, idx) => <i key={gig._id + idx} >{value ? <svg width="14.5" height="14.5" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path></svg> : <svg width="14.5" height="14.5" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M10.9327 9.18715L13.875 6.37996L9.81731 5.79395L8 2.18336L6.18269 5.79395L2.125 6.37996L5.06731 9.18715L4.36538 13.1664L8 11.2854L11.625 13.1664L10.9327 9.18715ZM16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7826 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path></svg>}</i>)}
+                                <span className="rate-num">{review.rate}</span>
+                            </div>
+
+                            <div className="review-txt">{review.txt} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore illo ipsum facere rerum, neque quia quasi nam error, non ab dolores! </div>
+
+                            <div className="helpful">
+                                <span className="title">Helpful?</span>
+                                <span className="yes">
+                                    <svg width="13" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M11.89 14.75H1C0.59 14.75 0.25 14.41 0.25 14V8C0.25 7.59 0.59 7.25 1 7.25H3.46L6.05 0.72C6.16 0.43 6.44 0.25 6.75 0.25H7.67C8.59 0.25 9.34 0.98 9.34 1.87V5.45H13.17C14 5.45 14.78 5.84 15.27 6.48C15.73 7.1 15.87 7.87 15.66 8.6L14.39 12.93C14.08 13.99 13.06 14.74 11.9 14.74L11.89 14.75ZM4.75 13.25H11.89C12.38 13.25 12.81 12.95 12.94 12.52L14.21 8.19C14.32 7.81 14.16 7.52 14.06 7.39C13.85 7.12 13.53 6.96 13.16 6.96H8.58C8.17 6.96 7.83 6.62 7.83 6.21V1.87C7.83 1.81 7.76 1.75 7.66 1.75H7.25L4.74 8.08V13.25H4.75ZM1.75 13.25H3.25V8.75H1.75V13.25V13.25Z"></path></svg>
+                                    Yes
+                                </span>
+                                <span className="no">
+                                    <svg width="13" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M9.25533 14.75H8.33533C7.41533 14.75 6.66533 14.03 6.66533 13.13L6.66533 9.55H2.83533C2.00533 9.55 1.22533 9.16 0.735326 8.52C0.275326 7.9 0.135326 7.13 0.345326 6.4L1.62533 2.06C1.93533 1 2.95533 0.25 4.11533 0.25L15.0053 0.25C15.4153 0.25 15.7553 0.59 15.7553 1V7C15.7553 7.41 15.4153 7.75 15.0053 7.75H12.5453L9.95533 14.28C9.84533 14.57 9.56533 14.75 9.25533 14.75ZM4.11533 1.75C3.62533 1.75 3.19533 2.05 3.06533 2.48L1.79533 6.81C1.68533 7.19 1.84533 7.48 1.94533 7.61C2.15533 7.88 2.47533 8.04 2.84533 8.04H7.42533C7.83533 8.04 8.17533 8.38 8.17533 8.79L8.17533 13.12C8.17533 13.17 8.24533 13.24 8.34533 13.24H8.75533L11.2653 6.91V1.75L4.11533 1.75ZM12.7553 6.25H14.2553V1.75L12.7553 1.75V6.25Z"></path></svg>
+                                    No
+                                </span>
+                            </div>
+
+
+                        </li>))}
+                </ul>
 
             </section>
 
