605e6e28e5241be660da89f875ea1a1f6aa04af4
Ben <bnk989@gmail.com>
2024-04-09 08:19:18 +0300

    minor pixsel perfect adjastment to homepage

--- src/cmps/TrustedBy.jsx
+++ src/cmps/TrustedBy.jsx
@@ -1,10 +1,10 @@
 export function TrustedBy(){
 
     return (
-        <section className="trusted-by bg-[#f6f9e5] mb-6">
-            <ul className="flex-center clean-list justify-center gap-3">
+        <section className="trusted-by bg-gray1 mb-24">
+            <ul className="flex-center clean-list justify-center gap-14 h-24">
                 <li className="flex-center text-gray-400 text-sm">Trusted by</li>
-                <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/fdbg9kvjrydeqaere8sn.png" alt="facebook logo" /></li>
+                <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/meta.12b5e5c.png" alt="meta logo" /></li>
                 <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/hgxyurzmqgdevqmg979v.png" alt="pg logo" /></li>
                 <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/ktprdhufw3xhhsrgd5ij.png" alt="netflix logo" /></li>
                 <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/sj7qx56f9tkb3vthvtbl.png" alt="google logo" /></li>
