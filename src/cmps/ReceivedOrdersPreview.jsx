c7ce5444f7885a42bb42d4cd1307bb5486c800ea
aviya-ha <aviyaharel6@gmail.com>
2024-04-14 15:18:12 +0300

    Fix orders

--- src/cmps/ReceivedOrdersPreview.jsx
+++ src/cmps/ReceivedOrdersPreview.jsx
@@ -2,21 +2,32 @@ import { useState } from "react"
 
 
 
-export function ReceivedOrdersPreview({order,index,onChangeAction,orders}) {
+export function ReceivedOrdersPreview({ order, index, onChangeAction, orders }) {
     const [isActionModalOpen, setActionModal] = useState(false)
 
-    function onActionBtn(value,idx){
+    function onActionBtn(value, idx) {
         let orderToUpdate = orders[idx]
         orderToUpdate.status = value
         onChangeAction(orderToUpdate)
         setActionModal(!isActionModalOpen)
     }
 
+    function GetDate(isoDate) {
+        // const formattedDate = format(new Date(isoDate), 'dd/MM/yy')
+        const date = new Date(isoDate);
+        const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear() % 100}`;
+        return formattedDate
+    }
+    // buyer
     return (<>
-        <td><img src={order.seller.imgUrl} alt="Avatar" /></td>
-        <td>{order.seller.fullname}</td>
-        <td>{order.gig.title}</td>
-        <td>{order.createdAt}</td>
+        <td><div className="buyer">
+            <img src={order.buyer.imgUrl} alt="Avatar" />{order.buyer.fullname}
+            </div></td>
+        {/* <td></td> */}
+        <td className="task">{order.gig.title}</td>
+
+        <td>{GetDate(order.createdAt)}</td>
+        {/* <td>{format(new Date(order.createdAt), 'dd/MM/yy')}</td> */}
         <td>${order.gig.price}</td>
         <td className={`received-order-action-container ${(order.status === "Accepted") && "accepted"} ${(order.status === "Rejected") && "rejected"}`} key={order._id} onClick={() => setActionModal(!isActionModalOpen)}><button>{order.status}</button></td>
         {
