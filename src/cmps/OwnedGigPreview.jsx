a5f5d6109dd4ebe7cfcb66a34fbec42505119cff
aviya-ha <aviyaharel6@gmail.com>
2024-04-14 15:45:51 +0300

    FIx empty gig

--- src/cmps/OwnedGigPreview.jsx
+++ src/cmps/OwnedGigPreview.jsx
@@ -7,7 +7,7 @@ export function OwnedGigPreview({ ownedGig }) {
             <img src={ownedGig?.imgUrls[0] || 'https://source.unsplash.com/random/307%C3%97408'} alt="gig-image" />
             <div>
                 <span>{ownedGig.title}</span>
-                <span className="price">$ {ownedGig.price}</span>
+                <span className="price">$ {(ownedGig.packages)? ownedGig.packages.basic.price : ownedGig.price }</span>
             </div>
 
         </>
