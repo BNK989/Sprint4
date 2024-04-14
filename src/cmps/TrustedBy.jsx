49f867e132f3b78db264fd977a05e2c1d2686568
Ben <bnk989@gmail.com>
2024-04-14 09:33:35 +0300

    trusted by section mobile friendly

--- src/cmps/TrustedBy.jsx
+++ src/cmps/TrustedBy.jsx
@@ -2,13 +2,13 @@ export function TrustedBy(){
 
     return (
         <section className="trusted-by bg-gray1 mb-24">
-            <ul className="flex-center clean-list justify-center gap-14 h-24">
-                <li className="flex-center text-gray-400 text-sm">Trusted by</li>
+            <ul className="flex-center clean-list justify-center gap-2 md:gap-14 h-24">
+                <li className="flex-center text-gray-400 text-sm hidden md:contents">Trusted by</li>
                 <li><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/meta.12b5e5c.png" alt="meta logo" /></li>
                 <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/hgxyurzmqgdevqmg979v.png" alt="pg logo" /></li>
                 <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/ktprdhufw3xhhsrgd5ij.png" alt="netflix logo" /></li>
                 <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/sj7qx56f9tkb3vthvtbl.png" alt="google logo" /></li>
-                <li><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/dnvcm1ob8c9r0azmzk2g.png" alt="paypal logo" /></li>
+                <li className="hidden md:contents"><img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793766/gigs/dnvcm1ob8c9r0azmzk2g.png" alt="paypal logo" /></li>
             </ul>
         </section>
     )
