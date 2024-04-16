d49990b8fa127c5e6e3334f87948beaf587653b9
aviya-ha <aviyaharel6@gmail.com>
2024-04-16 13:11:49 +0300

    More work on dashboard

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,42 +1,54 @@
-   
+
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-
+import { PieBestSellers } from '@/cmps/smallCmps/PieBestSellers';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 
 export function UserDashboard() {
-//     const data = {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [
-//           {
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//               'rgba(255, 99, 132, 0.2)',
-//               'rgba(54, 162, 235, 0.2)',
-//               'rgba(255, 206, 86, 0.2)',
-//               'rgba(75, 192, 192, 0.2)',
-//               'rgba(153, 102, 255, 0.2)',
-//               'rgba(255, 159, 64, 0.2)',
-//             ],
-//             borderColor: [
-//               'rgba(255, 99, 132, 1)',
-//               'rgba(54, 162, 235, 1)',
-//               'rgba(255, 206, 86, 1)',
-//               'rgba(75, 192, 192, 1)',
-//               'rgba(153, 102, 255, 1)',
-//               'rgba(255, 159, 64, 1)',
-//             ],
-//             borderWidth: 1,
-//           },
-//         ],
-//       };
-//   return (
-//   <Pie data={data} />;
-//   )
-return <>
-<h1>hey</h1></>
+
+  return (
+    <section className='UserDashboard main-container'>
+
+      <section className='basic-stats'>
+
+        <div className='earnings-to-date'>
+          <span className='title'>Earnings to date</span>
+          <span className='stat'>101,750$</span>
+        </div>
+
+        <div className='avg-selling-price'>
+          <span className='title'>Avg.selling price</span>
+          <span className='stat'>78$</span>
+        </div>
+
+        <div className='order-completion'>
+          <span className='title'>Order completion</span>
+          <span className='stat'>90%</span>
+        </div>
+
+        <div className='on-time-delivery'>
+          <span className='title'>On-time delivery</span>
+          <span className='stat' >97%</span>
+        </div>
+
+        <div className='orders-completed'>
+          <span className='title'>Orders completed</span>
+          <span className='stat'>97%</span>
+        </div>
+
+        <div className='earned-in-lest-month'>
+          <span className='title'>Earned in lest 30 days</span>
+          <span className='stat'>11,750$</span>
+        </div>
+      </section>
+      
+      <section className='charts'>
+          <PieBestSellers />
+      </section>
+    </section>
+  )
+
 }
