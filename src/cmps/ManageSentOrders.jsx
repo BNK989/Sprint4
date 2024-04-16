676322d510cf4b207c5948707d2a24f4d59c340a
meni-kr <meni.kramer@gmail.com>
2024-04-16 19:23:12 +0300

    Add background color to orders

--- src/cmps/ManageSentOrders.jsx
+++ src/cmps/ManageSentOrders.jsx
@@ -25,14 +25,14 @@ export function ManageSentOrders({ user, orders }) {
         {!orders.length &&
             <span className="no-orders">no orders</span>
         }
-        {orders.length &&
+        {!!orders.length &&
             <ul className="orders-list">
                 {orders.map(order => {
                     return <li key={order._id} className={gigStatusClass(order.status)}>
                         <img src={order.gig.imgUrl[0]} alt="" />
                         <span className="gig-title">{truncateStringToWordCount(order.gig.title , 5)}</span>
                         {/* <span className="price">${order.gig.price}</span> */}
-                        <span className="order-status">{order.status}</span>
+                        <span className={`order-status ${(order.status === 'pending')? 'pending' : 'approved'}`}>{order.status}</span>
                     </li>
                 })}
             </ul>
